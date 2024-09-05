341e46640f3a18021cf623f7a75bc09da6f64270
renato1958 <felisi.renato@gmail.com>
2024-09-05 18:53:26 +0200

    Abilitata connessione col DB

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import logger from "morgan";
 import helmet from "helmet";
 import { port } from "./config/config.js";
+import { connect2Db } from "./database/dbConnection.js";
+import mongoose from "mongoose";
 
 // Instantiate express app
 const app = express();
@@ -14,11 +16,18 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Connects to DB
+connect2Db();
+
 // GET route
 app.get("/", (req, res) => {
     res.send("<h1>Ciao, sono Arturo, il server che ce l'ha duro!</h1>");
 });
 
-app.listen(port, () => {
-    console.log(`Server in ascolto sulla porta ${port}.\nCTRL+C per terminare.`);
+mongoose.connection.once("open", () => {
+    console.log("Connessione col DB stabilita con successo");
+    
+    app.listen(port, () => {
+        console.log(`Server in ascolto sulla porta ${port}.\nCTRL+C per terminare.`);
+    });
 });
